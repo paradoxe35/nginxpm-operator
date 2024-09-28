d77a6c43894b02c2cbce5257305c1c5245c98321
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-09-28 10:24:59 +0000

    Add Token resource and controller to operator

---
+++ api/v1/token_types.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type Secret struct {
+	// SecretName is the name of the secret resource to add to the token cr
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type=string
+	// +required
+	SecretName string `json:"secretName"`
+}
+
+// TokenSpec defines the desired state of Token
+type TokenSpec struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Endpoint of a Nginx Proxy manager instance
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Pattern=`^https?://`
+	Endpoint string `json:"endpoint"`
+
+	// Secret resource reference to add to the token cr
+	// +required
+	Secret Secret `json:"secret,omitempty"`
+
+	// Expiration time of the token, value is generated from controller reconcile
+	// +kubebuilder:validation:Format=date-time
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type=string
+	// +optional
+	Expires *metav1.Time `json:"expires,omitempty"`
+
+	// Authentication Token, this is generated from controller reconcile
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type=string
+	// +optional
+	Token string `json:"token,omitempty"`
+}
+
+// TokenStatus defines the observed state of Token
+type TokenStatus struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Represents the observations of a Memcached's current state.
+	// Memcached.status.conditions.type are: "Available", "Progressing", and "Degraded"
+	// Memcached.status.conditions.status are one of True, False, Unknown.
+	// Memcached.status.conditions.reason the value should be a CamelCase string and producers of specific
+	// condition types may define expected values and meanings for this field, and whether the values
+	// are considered a guaranteed API.
+	// Memcached.status.conditions.Message is a human readable message indicating details about the transition.
+	// For further information see: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Token is the Schema for the tokens API
+type Token struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TokenSpec   `json:"spec,omitempty"`
+	Status TokenStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// TokenList contains a list of Token
+type TokenList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Token `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Token{}, &TokenList{})
+}
---
+++ internal/controller/token_controller.go
@@ -0,0 +1,116 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logger "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	nginxpmoperatoriov1 "github.com/paradoxe35/nginxpm-operator/api/v1"
+)
+
+const (
+	secretField = ".spec.secret.secretName"
+)
+
+// TokenReconciler reconciles a Token object
+type TokenReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=npmoperator.io,resources=tokens,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=npmoperator.io,resources=tokens/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=npmoperator.io,resources=tokens/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
+func (r *TokenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := logger.FromContext(ctx)
+
+	token := &nginxpmoperatoriov1.Token{}
+
+	if err := r.Get(ctx, req.NamespacedName, token); err != nil {
+		log.Error(err, "Unable to fetch Token")
+		return ctrl.Result{}, err
+	}
+
+	// if token.Spec.Expires.Before(&metav1.Now()) {
+	// 	log.Info("Token is expired")
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&nginxpmoperatoriov1.Token{}).
+		Owns(&nginxpmoperatoriov1.Token{}).
+		Watches(
+			&corev1.Secret{},
+			handler.EnqueueRequestsFromMapFunc(r.findObjectsForSecret),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{}),
+		).
+		Complete(r)
+}
+
+func (r *TokenReconciler) findObjectsForSecret(ctx context.Context, secret client.Object) []reconcile.Request {
+	attachedSecrets := &nginxpmoperatoriov1.TokenList{}
+
+	listOps := &client.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector(secretField, secret.GetName()),
+		Namespace:     secret.GetNamespace(),
+	}
+
+	err := r.List(ctx, attachedSecrets, listOps)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(attachedSecrets.Items))
+	for i, item := range attachedSecrets.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      item.GetName(),
+				Namespace: item.GetNamespace(),
+			},
+		}
+	}
+
+	return requests
+}
