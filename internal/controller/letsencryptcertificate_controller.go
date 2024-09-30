19c6fc7f9f26290a07609a1d567e17bd3aad9259
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-09-30 23:01:27 +0000

    Fix typo in CRD and update RBAC permissions
    Corrected 'providerCredential' to 'providerCredentials' in CRD to maintain consistency with the schema definition. Updated RBAC permissions to include 'secrets' for Let's Encrypt certificates to ensure the controller has the necessary access to manage secrets appropriately.

--- internal/controller/letsencryptcertificate_controller.go
+++ internal/controller/letsencryptcertificate_controller.go
@@ -36,6 +36,7 @@ type LetsEncryptCertificateReconciler struct {
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=letsencryptcertificates,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=letsencryptcertificates/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=letsencryptcertificates/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
--- internal/controller/token_controller.go
+++ internal/controller/token_controller.go
@@ -54,7 +54,7 @@ type TokenReconciler struct {
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=tokens,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=tokens/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=nginxpm-operator.io,resources=tokens/finalizers,verbs=update
-// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
